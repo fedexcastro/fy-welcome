414b2806536ed5180362a063e9792cad2215bb09
fedexcastrp <fmc.0208@gmail.com>
2015-01-29 21:36:36 -0300

    Welcome changes:
    1) Now the interval between products is lower.
    2) Now the forget yesterday logo has infinite movement.
    3) Now the width and height of the page is 100%.

--- welcome/animation.js
+++ welcome/animation.js
@@ -45,23 +45,25 @@ function animate (){
     leftSlider.append('<a id="left-product-link" href="' + leftItem.href + '"><img  src="' + leftItem.img1 + '" class="img-top img-responsive" id="left-top-img">' + '<img src="' + leftItem.img2 + '" class="img-bottom img-responsive" id="left-bottom-img">'+'</a>');
     rightSlider.append('<a id="right-product-link" href="' + rightItem.href + '"><img src="' + rightItem.img1 + '" class="img-top img-responsive" id="right-top-img">' + '<img src="' + rightItem.img2 + '" class="img-bottom img-responsive" id="right-bottom-img">'+'</a>');
 
-    $("#left-top-img").animate({bottom: "0"}, 2000);
-    $("#left-bottom-img").animate({top: "0"}, 2000);
-    $("#right-top-img").animate({bottom: "0"}, 2000);
-    $("#right-bottom-img").animate({top: "0"}, 2000);
+    var timerVertical = 2000;
+    var timerHorizontal = 2000;
+    $("#left-top-img").animate({bottom: "0"}, timerVertical);
+    $("#left-bottom-img").animate({top: "0"}, timerVertical);
+    $("#right-top-img").animate({bottom: "0"}, timerVertical);
+    $("#right-bottom-img").animate({top: "0"}, timerVertical);
 
     setTimeout(function(){
-        $("#left-top-img").animate({right: "300%"}, 2000);
-        $("#left-bottom-img").animate({right: "300%"}, 2000);
-        $("#right-top-img").animate({left: "300%"}, 2000);
-        $("#right-bottom-img").animate({left: "300%"}, 2000);
-    }, 5000);
+        $("#left-top-img").animate({right: "400%"}, timerHorizontal);
+        $("#left-bottom-img").animate({right: "400%"}, timerHorizontal);
+        $("#right-top-img").animate({left: "400%"}, timerHorizontal);
+        $("#right-bottom-img").animate({left: "400%"}, timerHorizontal);
+    }, 3000);
 
     setTimeout(function(){
         $('#left-product-link').remove();
         $('#right-product-link').remove();
-    }, 7000);
+    }, 3800);
 }
 
-$(function(){animate(); setInterval(animate, 8000);});
+$(function(){animate(); setInterval(animate, 3900);});
 
